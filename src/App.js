7844701dff7d17f944a82b821b84ced4bac15491
drisha1 <drisha.sahby@gmail.com>
2025-01-28 22:10:05 +0100

    update HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { MainLayout } from './components/layout/MainLayout';
 import { Dashboard } from './components/modules/Dashboard';
 import { AssetList } from './components/modules/assets/AssetList';
